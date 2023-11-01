0cad149985560858f3b28feeaca9040b2516760d
Carlos Waldo Rojas <krlosw9@gmail.com>
2023-11-01 06:10:30 -0500

    Enviado el js de envio de formulario de contacto a un archivo js separado del html y descomentado google maps

--- js/index.js
+++ js/index.js
@@ -9,3 +9,52 @@ toggleMenuElement.addEventListener('click', () => {
   mainNav.classList.toggle('move-content');
   logo.classList.toggle('hide-content');
 })
+
+
+document.getElementById('form-send-mail').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Obtener los datos del formulario
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const address = document.getElementById('address').value;
+  const issue = document.getElementById('issue').value;
+  const message = document.getElementById('message').value;
+
+  // Crear un objeto con los datos del formulario
+  const data = {
+    name: name,
+    email: email,
+    phone: phone,
+    address: address,
+    issue: issue,
+    messageContact: message
+  };
+  if(!name || !email || !phone){
+    alert("Los campos con * son obligatorios");
+    return;
+  }
+  
+  fetch('https://innovasit.com/api/api/send-email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => {
+      alert(data.message);
+      
+    })
+    .catch(error => {
+      alert("Ocurrió un error al enviar correo");
+    });
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phone').value = '';
+    document.getElementById('address').value = '';
+    document.getElementById('issue').value = '';
+    document.getElementById('message').value = '';
+});
